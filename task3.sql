742fb607b67cdfde8dd52161d9764567b13337ba
German-V <gvaschelin@mail.ru>
2024-01-17 18:45:12 +0300

    Task3

--- task3.sql
+++ task3.sql
@@ -1,9 +1,10 @@
---1
+--1 Вывести количество фильмов в каждой категории, отсортировать по убыванию.
 select category, count(*) as film_count from film_list fl 
 group by category
+order by film_count desc
 
 
---2
+--2 Вывести 10 актеров, чьи фильмы большего всего арендовали, отсортировать по убыванию.
 select actor_id, (
 select sum(rental_duration) from film f
 join film_actor fa on f.film_id=fa.film_id
@@ -15,15 +16,21 @@ order by rent_sum desc
 limit 10
 
 
---3
-select distinct category, sum(price) over(partition by category) from film_list fl 
+--3 Вывести категорию фильмов, на которую потратили больше всего денег.
+select distinct category, sum(price) over(partition by category) sum from film_list fl 
 order by sum desc
-limit 1
+fetch first row with ties
 
---4
+--4 Вывести названия фильмов, которых нет в inventory. Написать запрос без использования оператора IN.
 select distinct title from film f
-join inventory i on f.film_id = i.film_id 
-where exists (select film_id from inventory) 
+where not exists (select film_id from inventory i where f.film_id = i.film_id) 
+--без подзапроса
+
+select distinct title from film f
+left join inventory i on f.film_id = i.film_id
+where i.film_id is null
+
+
 
 
 --5
