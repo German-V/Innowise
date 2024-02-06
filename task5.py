ca8e8302ec9645054691679b8591086b6a58ef07
German-V <gvaschelin@mail.ru>
2024-02-06 14:42:34 +0300

    fixed queries

--- task5.py
+++ task5.py
@@ -3,12 +3,14 @@ from configparser import ConfigParser
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
-from pyspark.sql.window import Window
-from pyspark.sql.functions import dense_rank, rank, row_number
+from pyspark.sql.functions import row_number, dense_rank
 from pyspark.sql.functions import desc
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, when
+from pyspark.sql.functions import max, avg, min, count, sum, expr
+
 
 spark = SparkSession.builder.master('local[*]').appName("task5").config('spark.jars', 'postgresql-42.7.1.jar').getOrCreate()
+
 
 def get_table(properties: dict, url: str, table_name: str, spark: SparkSession):
     return spark.read.jdbc(
@@ -45,88 +47,85 @@ df_film_category = get_table(properties, connection_str, 'film_category', spark)
 # %%
 df_film_actor = get_table(properties, connection_str, 'film_actor', spark)
 
-
 # %%
-df_film_list.createOrReplaceTempView("film_list")
-df_film.createOrReplaceTempView("film")
-df_actor.createOrReplaceTempView("actor")
-df_film_actor.createOrReplaceTempView("film_actor")
-df_inventory.createOrReplaceTempView("inventory")
-df_category.createOrReplaceTempView("category")
-df_film_category.createOrReplaceTempView("film_category")
-df_city.createOrReplaceTempView("city")
-df_customer.createOrReplaceTempView("customer")
-df_rental.createOrReplaceTempView("rental")
-df_address.createOrReplaceTempView("address")
-
-# %%
-spark.sql("select category, count(*) as film_count from film_list fl \
-group by category \
-order by film_count desc \
-").collect()
+df_film_list.groupBy("category").agg(count("*").alias("film_count")).show()
 
 
 # %%
-spark.sql("select actor_id, (\
-    select sum(rental_duration) from film f\
-    join film_actor fa on f.film_id=fa.film_id\
-    where fa.actor_id = a.actor_id\
-    group by fa.actor_id\
-    ) as rent_sum\
-    from actor a\
-    order by rent_sum desc \
-    limit 10").collect()
+joined_df = df_film.alias("f").join(df_film_actor.alias("fa"), col("f.film_id") == col("fa.film_id"), 'inner')
+
+
+df_actor.alias("a")\
+.select("a.actor_id")\
+.join(joined_df, col("a.actor_id") == col("fa.actor_id"), 'inner')\
+.groupBy("a.actor_id")\
+.agg(sum(col("f.rental_duration")).alias('rent_sum'))\
+.orderBy(col("rent_sum").desc())\
+.limit(10).show()
+
 
 
 # %%
 
-spark.sql("select distinct category, sum(price) over(partition by category) sum_price from film_list fl \
-order by sum_price desc").withColumn("num", dense_rank().over(Window.orderBy(desc("sum_price")))).filter("num=1").show()
+df_film_list\
+.select(col('category'), 
+        sum(col("price")).over(Window().partitionBy("category")).alias("sum_price"))\
+.orderBy(col('sum_price').desc()).distinct()\
+.withColumn("num", dense_rank().over(Window.orderBy(desc("sum_price")))).filter("num=1").show()
 
 
 # %%
-spark.sql("select distinct title from film f \
-where not exists (select film_id from inventory i where f.film_id = i.film_id) ").show()
+df_film.alias("f").join(df_inventory.alias("i"), col("f.film_id")==col("i.film_id"), 'anti')\
+    .select("f.title").distinct().show()
 
 # %%
-spark.sql("select first_name, last_name, total from( \
-select dense_rank() over(order by count(a.actor_id) desc) dn, a.first_name, a.last_name, count(a.actor_id) as total  from actor a \
-join film_actor fa on a.actor_id = fa.actor_id \
-join film_category fc on fa.film_id = fc.film_id \
-join category c on c.category_id =fc.category_id \
-where c.name = 'Children' \
-group by a.first_name,a.last_name ) b \
-where dn <= 3").show()
+df_temp = df_actor.alias("a")\
+    .join(df_film_actor.alias("fa"), col("a.actor_id") == col("fa.actor_id"))\
+    .join(df_film_category.alias("fc"), col("fa.film_id") == col("fc.film_id"))\
+    .join(df_category.alias("c"), col("c.category_id") == col("fc.category_id"))\
+    .select("a.first_name", "a.last_name","a.actor_id")\
+    .where(col("c.name")=='Children')\
+    .groupBy("a.first_name","a.last_name").agg(count("a.actor_id").alias('total'))\
+    .withColumn("dn", dense_rank().over(Window.orderBy(col('total').desc())).alias('dn'))
+df_temp.select("first_name", "last_name", "total")\
+    .where(col("dn")<=3).show()
 
 # %%
-spark.sql("select city, count(c2.customer_id), sum(c2.active) active, count(*) filter(where c2.active=0) inactive from city c \
-left join address a on a.city_id = c.city_id \
-join customer c2 ON a.address_id = c2.address_id \
-group by city \
-order by inactive desc").show()
+df_city.alias("c")\
+    .join(df_address.alias("a"), col("a.city_id")==col("c.city_id"), 'left')\
+    .join(df_customer.alias("c2"), col("a.address_id")==col("c2.address_id"), 'inner')\
+    .select("city", "c2.customer_id", "c2.active")\
+    .groupBy("city")\
+    .agg(count("c2.customer_id").alias("customer_count"),
+        sum("c2.active").alias("active"),
+        count(expr("CASE WHEN c2.active = 0 THEN 1 END")).alias("inactive"))\
+    .orderBy(col("inactive").desc()).show()
 
 # %%
-spark.sql("select name, rent from(( \
-select name, sum(r.return_date-r.rental_date) rent from category c \
-join film_category fc on fc.category_id  = c.category_id \
-join inventory i on i.film_id = fc.film_id \
-join rental r on r.inventory_id = i.inventory_id \
-join customer c2 on c2.customer_id =r.customer_id \
-join address a on a.address_id = c2.address_id \
-join city c3 on c3.city_id =a.city_id and c3.city like 'a%' \
-group by name \
-order by rent desc \
-limit 1) \
-union \
-(select name, sum(r.return_date-r.rental_date) rent from category c \
-join film_category fc on fc.category_id  = c.category_id \
-join inventory i on i.film_id = fc.film_id \
-join rental r on r.inventory_id = i.inventory_id \
-join customer c2 on c2.customer_id =r.customer_id \
-join address a on a.address_id = c2.address_id \
-join city c3 on c3.city_id =a.city_id and c3.city like '%-%' \
-group by name \
-order by rent desc \
-limit 1))").show()
+df_category.alias("c")\
+.join(df_film_category.alias("fc"), col("fc.category_id") == col("c.category_id"))\
+.join(df_inventory.alias("i"), col("i.film_id") == col("fc.film_id"))\
+.join(df_rental.alias("r"), col("r.inventory_id") == col("i.inventory_id"))\
+.join(df_customer.alias("c2"), col("c2.customer_id") == col("r.customer_id"))\
+.join(df_address.alias("a"), col("a.address_id") == col("c2.address_id"))\
+.join(df_city.alias("c3"), (col("c3.city_id") == col("a.city_id")) & (col("c3.city").like('a%')))\
+.groupBy("c.name")\
+.agg(sum(col("r.return_date") - col("r.rental_date")).alias("rent"))\
+.orderBy(col("rent").desc())\
+.limit(1)\
+.union(
+    df_category.alias("c")
+    .join(df_film_category.alias("fc"), col("fc.category_id") == col("c.category_id"))
+    .join(df_inventory.alias("i"), col("i.film_id") == col("fc.film_id"))
+    .join(df_rental.alias("r"), col("r.inventory_id") == col("i.inventory_id"))
+    .join(df_customer.alias("c2"), col("c2.customer_id") == col("r.customer_id"))
+    .join(df_address.alias("a"), col("a.address_id") == col("c2.address_id"))
+    .join(df_city.alias("c3"), (col("c3.city_id") == col("a.city_id")) & (col("c3.city").like('%-%')))
+    .groupBy("c.name")
+    .agg(sum(col("r.return_date") - col("r.rental_date")).alias("rent"))
+    .orderBy(col("rent").desc())
+    .limit(1)
+    )\
+.show()
 
 
